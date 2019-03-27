7170d10bce993667c96c4ec13183b2ad2b524031
wetechdomain <we.techdoma.in@gmail.com>
2019-03-27 19:30:13 +0530

    1 file modified, 3 deleted

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
 import Demo1 from './Demo1';
-import Demo2 from './Demo2';
 import './style.css';
 
 class App extends Component {
@@ -10,7 +9,6 @@ class App extends Component {
     return(
       <div>
         <Demo1/>
-        <Demo2/>
       </div>
     )
   }
